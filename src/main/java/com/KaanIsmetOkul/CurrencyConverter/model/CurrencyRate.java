c2ea47dea0ae534e2b5307d23402c81889383b95
KaanIsmet <kaanismet7@gmail.com>
2024-09-11 19:38:59 +0300

    added CurrencyRate

--- src/main/java/com/KaanIsmetOkul/CurrencyConverter/model/CurrencyRate.java
+++ src/main/java/com/KaanIsmetOkul/CurrencyConverter/model/CurrencyRate.java
@@ -1,9 +1,19 @@
 package com.KaanIsmetOkul.CurrencyConverter.model;
 
 
+import org.springframework.stereotype.Component;
 
-
+@Component
 public class CurrencyRate {
-    private String currency;
+    private String fromCurrency;
+    private double amount;
+    private String toCurrency;
+    private double currencyRate;
 
+    public CurrencyRate(String fromCurrency, double amount, String toCurrency, double currencyRate) {
+        this.fromCurrency = fromCurrency;
+        this.amount = amount;
+        this.toCurrency = toCurrency;
+        this.currencyRate = currencyRate;
+    }
 }
