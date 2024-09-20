b7e8495e4c9a5a4262a03f92dfc661fb41881c23
KaanIsmet <kaanismet7@gmail.com>
2024-09-20 14:06:07 +0300

    added styling to Conversion page, and implement CurrencyRate

--- src/main/java/com/KaanIsmetOkul/CurrencyConverter/model/CurrencyRate.java
+++ src/main/java/com/KaanIsmetOkul/CurrencyConverter/model/CurrencyRate.java
@@ -1,7 +1,7 @@
 package com.KaanIsmetOkul.CurrencyConverter.model;
-
-
 import org.springframework.stereotype.Component;
+import java.lang.String;
+
 
 @Component
 public class CurrencyRate {
@@ -10,10 +10,52 @@ public class CurrencyRate {
     private String toCurrency;
     private double currencyRate;
 
+    public CurrencyRate() {}
+
     public CurrencyRate(String fromCurrency, double amount, String toCurrency, double currencyRate) {
         this.fromCurrency = fromCurrency;
         this.amount = amount;
         this.toCurrency = toCurrency;
         this.currencyRate = currencyRate;
+    }
+
+    public String getFromCurrency() {
+        return fromCurrency;
+    }
+
+    public void setFromCurrency(String fromCurrency) {
+        this.fromCurrency = fromCurrency;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getToCurrency() {
+        return toCurrency;
+    }
+
+    public void setToCurrency(String toCurrency) {
+        this.toCurrency = toCurrency;
+    }
+
+    public double getCurrencyRate() {
+        return currencyRate;
+    }
+
+    public void setCurrencyRate(double currencyRate) {
+        this.currencyRate = currencyRate;
+    }
+
+    @Override
+    public String toString() {
+        return "fromCurrency = " + fromCurrency + "\n" +
+                "toCurrency = " + toCurrency + "\n" +
+                "amount = " + Double.toString(amount) + "\n" +
+                "double Rate = " + Double.toString(currencyRate);
     }
 }
