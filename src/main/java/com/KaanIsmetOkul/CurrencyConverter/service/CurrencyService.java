8b729c39474a8e7d2aad287da74fded2fcc00f52
KaanIsmet <kaanismet7@gmail.com>
2024-09-26 23:45:42 +0300

    added convert method to currencyService

--- src/main/java/com/KaanIsmetOkul/CurrencyConverter/service/CurrencyService.java
+++ src/main/java/com/KaanIsmetOkul/CurrencyConverter/service/CurrencyService.java
@@ -1,10 +1,21 @@
 package com.KaanIsmetOkul.CurrencyConverter.service;
 
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 
 @Service
 public class CurrencyService {
@@ -18,9 +29,31 @@ public class CurrencyService {
     @Value("${api.key}")
     private String apiKey;
 
-    /*public double convert(double amount, String fromCurrency, String toCurrency){
-        //convert one currency to another
-    }*/
+    public double convert(String url){
+
+        double result = 0.0;
+        try {
+            URI apiURI = new URI(url);
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(apiURI)
+                    .header("Authorization", "Bearer" + apiKey)
+                    .build();
+
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            if (response.statusCode() == 200) {
+                ObjectMapper objectMapper = new ObjectMapper();
+                JsonNode node = objectMapper.readTree(response.body());
+            }
+        } catch (IOException | URISyntaxException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        return result; //placeholder
+    }
+
+
 
     public String getApiUrl() {
         return apiUrl;
